3cb8734068ce8ace7ab8edb45f010daf21a9cc43
전정민 <sser7602@naver.com>
2026-09-01 02:53:19 +0900

    feat: CRUD 게시판 서버 만들기 추가

---
+++ CRUD/src/main/java/com/example/CRUD/CRUDRepository.java
@@ -0,0 +1,9 @@
+package com.example.CRUD;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CRUDRepository extends JpaRepository<CRUD, Integer> {
+    
+}
